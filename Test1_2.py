35586c7dca637f20d1adb8631db010cb84560366
hongduykandy <86794653+hongduykandy@users.noreply.github.com>
2024-03-12 10:25:10 +0900

    Add files via upload

---
+++ Test1_2.py
@@ -0,0 +1,14 @@
+a = int(input("Enter value a: "))
+b = int(input("Enter value b: "))
+
+result = a+b
+print(a,"+",b, "=", result)
+
+result = a-b
+print(a,"-",b, "=", result)
+
+result = a*b
+print(a,"*",b, "=", result)
+
+result = a/b
+print(a,"/",b, "=", result)
